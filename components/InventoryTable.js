bd049cd4fa6719525297ed41fc5a666bc2d868c5
Archan Patel <archanmehulpatel@gmail.com>
2024-03-19 21:08:26 -0400

    Added refresh button to live inventory page

--- components/InventoryTable.js
+++ components/InventoryTable.js
@@ -1,27 +1,31 @@
 "use client";
-
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const InventoryTable = () => {
     const [inventoryData, setInventoryData] = useState([]);
 
-    useEffect(() => {
-        const fetchInventoryData = async () => {
-            try {
-                const response = await fetch('/api/getInventory');
-                const data = await response.json();
-                setInventoryData(data);
-            } catch (error) {
-                console.error("Error fetching inventory data:", error);
-            }
-        };
+    const fetchInventoryData = async () => {
+        try {
+            const response = await fetch('/api/getInventory');
+            const data = await response.json();
+            setInventoryData(data);
+        } catch (error) {
+            console.error("Error fetching inventory data:", error);
+        }
+    };
 
+    useEffect(() => {
         fetchInventoryData();
     }, []);
 
     return (
         <div>
             <h1 style={{ textAlign: 'center', fontSize: '2em', fontWeight: 'bold' }}>Inventory</h1>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button type='button' onClick={fetchInventoryData} className='black_btn'>
+                    Refresh
+                </button>
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -40,7 +44,6 @@ const InventoryTable = () => {
             </table>
         </div>
     );
-    
 };
 
 export default InventoryTable;
